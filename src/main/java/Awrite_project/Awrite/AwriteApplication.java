c569d33b05e2ddc04347d77892e1035453618792
chock-cho <christer10@ewhain.net>
2024-01-08 19:52:09 +0900

    초기 환경 세팅

---
+++ src/main/java/Awrite_project/Awrite/AwriteApplication.java
@@ -0,0 +1,13 @@
+package Awrite_project.Awrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwriteApplication.class, args);
+	}
+
+}
