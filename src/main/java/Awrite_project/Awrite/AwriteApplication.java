f02172faff510fa673e6f32e1f555f727d963d9f
jjin70 <jjin0434@naver.com>
2024-02-10 22:55:10 +0900

    fix: swagger 설정

--- src/main/java/Awrite_project/Awrite/AwriteApplication.java
+++ src/main/java/Awrite_project/Awrite/AwriteApplication.java
@@ -1,9 +1,13 @@
 package Awrite_project.Awrite;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080/", description = "Default Server URL")})
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
