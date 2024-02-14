5e562f34b8ef314116ced10e1984023d83198d16
jjin70 <jjin0434@naver.com>
2024-02-15 00:22:58 +0900

    fix: SpringBootTest 어노테이션 제거

--- src/test/java/Awrite_project/Awrite/AwriteApplicationTests.java
+++ src/test/java/Awrite_project/Awrite/AwriteApplicationTests.java
@@ -3,7 +3,7 @@ package Awrite_project.Awrite;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AwriteApplicationTests {
 
 	@Test
