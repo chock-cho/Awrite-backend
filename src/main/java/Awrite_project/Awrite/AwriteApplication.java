d632ba4318720bc5d0bb5acdc7f084b0313d7db3
jjin70 <jjin0434@naver.com>
2024-02-16 22:40:46 +0900

    fix: Swagger API 서버 수정

--- src/main/java/Awrite_project/Awrite/AwriteApplication.java
+++ src/main/java/Awrite_project/Awrite/AwriteApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://localhost:8080/", description = "Default Server URL")})
-
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 public class AwriteApplication {
