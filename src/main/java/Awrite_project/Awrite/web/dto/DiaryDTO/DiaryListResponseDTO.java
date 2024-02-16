953095ac96248992062f65b76cc443a944a9f08a
jjin70 <jjin0434@naver.com>
2024-02-17 00:49:20 +0900

    fix: 일기 DTO 추가

--- src/main/java/Awrite_project/Awrite/web/dto/DiaryDTO/DiaryListResponseDTO.java
+++ src/main/java/Awrite_project/Awrite/web/dto/DiaryDTO/DiaryListResponseDTO.java
@@ -10,20 +10,24 @@ import lombok.Setter;
 @NoArgsConstructor
 /* Home 화면에서 보일 일기 리스트 */
 public class DiaryListResponseDTO {
+    private long diaryId;
     private String title;
     private String content;
     private String imgUrl;
     private boolean secret; // 비밀글 여부
     private String authorName; // 작성자 이름
     private int authorProfile; // 작성자 프로필 사진
+    private boolean heartby; // 로그인한 사용자가 좋아요 눌렀는
 
     public DiaryListResponseDTO(Diary diary){
+        this.diaryId = diary.getId();
         this.title = diary.getTitle();
         this.content = diary.getContent();
         this.imgUrl = diary.getImgUrl();
         this.secret = diary.isSecret();
         this.authorName = (diary.getAuthor() != null) ? diary.getAuthor().getNickname() : null;
         this.authorProfile = (diary.getAuthor() != null) ? diary.getAuthor().getProfilePicture() : 1;
+        this.heartby = diary.isHeartBy(diary.getAuthor());
     }
 
 }
